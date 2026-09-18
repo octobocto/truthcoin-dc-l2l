536a73becfee828fcaa9551df66416d6296e1fac
octobocto <bo@jalborg.com>
2026-09-17 21:05:25 -0700

    node: disconnect from the parent's event block
    The 2WPD boundary skipped one block, so a reorg across a deposit hit the
    deposit-height assert in State::disconnect.

--- integration_tests/integration_test.rs
+++ integration_tests/integration_test.rs
@@ -14,7 +14,7 @@ use plain_bitassets_app_rpc_api::node::RpcClient as _;
 use crate::{
     block_index::block_index_trial,
     block_template::block_template_trial,
-    ibd::ibd_trial,
+    ibd::{ibd_trial, reorg_across_deposit_trial},
     list_mempool::list_mempool_trial,
     receive_address::receive_address_trial,
     setup::{Init, PostSetup},
@@ -184,6 +184,11 @@ pub fn tests(
             failure_collector.clone(),
         ),
         ibd_trial(
+            bin_paths.clone(),
+            file_registry.clone(),
+            failure_collector.clone(),
+        ),
+        reorg_across_deposit_trial(
             bin_paths.clone(),
             file_registry.clone(),
             failure_collector.clone(),
--- lib/node/net_task.rs
+++ lib/node/net_task.rs
@@ -218,7 +218,7 @@ pub(in crate::node) fn disconnect_tip_(
             .rev_iter(rwtxn)
             .map_err(DbError::from)?
             .find_map(|(_, (block_hash, applied_height))| {
-                if applied_height < height - 1 {
+                if applied_height < height {
                     Ok(Some((block_hash, applied_height)))
                 } else {
                     Ok(None)
@@ -228,7 +228,7 @@ pub(in crate::node) fn disconnect_tip_(
             .withdrawal_bundle_event_blocks()
             .rev_iter(rwtxn)?
             .find_map(|(_, (block_hash, applied_height))| {
-                if applied_height < height - 1 {
+                if applied_height < height {
                     Ok(Some((block_hash, applied_height)))
                 } else {
                     Ok(None)
